c803cf49a8f4a7285e44f0a9956aedb7f685d56f
Allan <nurulla@gmail.com>
2016-03-01 00:02:31 -0500

    GroupHelper class created, related to groups classes replaced to GroupHelper

--- addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,10 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.goToGroupPage();
-    app.initGroupCreation();
-    app.fillGroupForm(new GroupData("test1", "test11", "test111"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test11", "test111"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }
