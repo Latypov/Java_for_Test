47cc5febc9046be4bafbd71430b216be0c3f9ff6
Allan <nurulla@gmail.com>
2016-03-17 15:10:03 -0400

    Added check by groups lists size

--- addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import my.javapr.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * Created by allan on 3/2/2016.
  */
@@ -15,14 +17,14 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test2", null, null));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test21", "test22"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
