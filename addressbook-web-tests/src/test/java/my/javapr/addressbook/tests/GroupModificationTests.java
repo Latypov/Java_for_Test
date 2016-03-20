b9cbdd5e07cdbcf04dc9acccef4918a1fcb4d4be
Allan <nurulla@gmail.com>
2016-03-20 13:36:54 -0400

    added comparison of groups using of their full lists

--- addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import my.javapr.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -20,11 +21,17 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test21", "test22"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test21", "test22");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1) ;
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 
 }
