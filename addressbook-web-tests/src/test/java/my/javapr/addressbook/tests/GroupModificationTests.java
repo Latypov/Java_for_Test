e0158e3c19f7ae6736b12ead06c972c267703124
Allan <nurulla@gmail.com>
2016-03-21 14:31:31 -0400

    Group tests modified to fluent interface

--- addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/my/javapr/addressbook/tests/GroupModificationTests.java
@@ -17,7 +17,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("test2", null, null));
+      app.group().create(new GroupData().withName("test2"));
     }
   }
 
@@ -25,7 +25,8 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(), "test1", "test21", "test22");
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId()).withName("test1").withHeader("test21").withFooter("test22");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
